e53fe887bb6596b018d34220f8e53ca80809a710
Joona Yoon <joonas.yoon@gmail.com>
2018-01-24 18:54:58 +0000

    관리자 사이트 메뉴에 추가

--- dmoj/settings.py
+++ dmoj/settings.py
@@ -61,6 +61,7 @@ else:
                     'children': [
                         'judge.ProblemGroup',
                         'judge.ProblemType',
+                        'judge.ProblemSource',
                     ],
                 },
                 {
